f5e84342eb70ab3b7cce3b23c3fd707ad3fe4a51
dhian <dhianpra12vio@gmail.com>
2024-10-01 09:34:37 +0800

    first commit
    base code

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,29 @@
+import Menu from "@/components/Menu";
+import { Didact_Gothic } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DahsboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) { 
+  return (
+    <div className="h-screen flex">
+      {/*LEFT*/}
+      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] rc={item.icon} alt="" width={20} height={20}/>'>
+          <Link 
+          href="/" 
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} className="block" />
+          <span className="hidden lg:block">BAKUDAPA</span>
+        </Link>
+        <Menu/>
+      </div>
+      {/*RIGHT*/}
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]'>r</div>
+    </div>
+    
+  );
+}
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,25 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map(i=>(
+        <div className="flex flex-col gap-2 " key={i.title}>
+          <span className="hidden lg:block text-grey-400 font-bold my-4">{i.title}</span>
+          {i.items.map(item=>(
+            <Link href={item.href} key={item.label}>
+            <Image src={item.icon} alt="" width={20} height={20}/>
+            <span>{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      
+
+      ))}
+    </div>
+  )
+}
+
+export default Menu
